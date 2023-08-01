295ac2ecb6b106ba7619bfe2e7fc345cc0d5b5d4
Roid-obi <contohdiamazon2@gmail.com>
2023-08-01 11:42:03 +0700

    feat: create navbar

--- src/stories/Header.stories.ts
+++ src/stories/Header.stories.ts
@@ -19,7 +19,7 @@ type Story = StoryObj<typeof meta>;
 export const LoggedIn: Story = {
   args: {
     user: {
-      name: 'Jane Doe',
+      name: 'Human',
     },
   },
 };
--- src/stories/Header.tsx
+++ src/stories/Header.tsx
@@ -34,12 +34,12 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }: HeaderProps
             <span className="welcome">
               Welcome, <b>{user.name}</b>!
             </span>
-            <Button size="small" onClick={onLogout} label="Log out" />
+            {/* <Button size="small" onClick={onLogout} label="Log out" /> */}
           </>
         ) : (
           <>
-            <Button size="small" onClick={onLogin} label="Log in" />
-            <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
+            <Button size="small" onClick={onCreateAccount} label="Daftar" />
+            <Button size="small" onClick={onLogin} label="Masuk" />
           </>
         )}
       </div>
