8fed14dbe97f15ec3b35a86d23e3e7e2057412bb
Roid-obi <contohdiamazon2@gmail.com>
2023-08-04 09:18:47 +0700

    delete: delete unnecessary files

--- src/stories/From/Form.stories.ts
+++ src/stories/From/Form.stories.ts
@@ -1,14 +1,14 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { Form } from './Form';
+import type { Meta, StoryObj } from "@storybook/react";
+import { Form } from "./Form";
 
 const meta = {
-  title: 'Example/Form',
+  title: "Stories/Form",
   component: Form,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
+    layout: "fullscreen",
   },
 } as Meta<typeof Form>;
 
@@ -20,6 +20,5 @@ Default.args = {};
 
 export const WithPlaceholder: Story = {};
 WithPlaceholder.args = {
-  placeholder: 'Enter your text...',
+  placeholder: "Enter your Text...",
 };
-
