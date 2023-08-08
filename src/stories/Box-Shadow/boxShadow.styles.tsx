fa0a70a3855051834a975a67b55b0f22847f73aa
Roid-obi <contohdiamazon2@gmail.com>
2023-08-08 17:04:21 +0700

    change: comment indonesian to english

--- src/stories/Box-Shadow/boxShadow.styles.tsx
+++ src/stories/Box-Shadow/boxShadow.styles.tsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
-const Boxstyled = styled.div`
-  /* CSS styling untuk kelas "storybook-button" */
+const BoxStyled = styled.div`
+  /* CSS styling for the "storybook-box-shadow" class */
   &.storybook-box {
-    /* Gaya umum untuk tombol */
+    /* General style for box */
     font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
     background-color: white;
     color: #0000008a;
@@ -20,35 +20,37 @@ const Boxstyled = styled.div`
   }
 
 
-  /* Gaya untuk box shadow base */
+  /* Style for box shadow base */
   &.storybook-box--base {
     box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
   }
 
-  /* Gaya untuk box shadow Md */
+  /* Style for box shadow Md */
   &.storybook-box--Md {
     box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
   }
 
-  /* Gaya untuk box shadow Lg */
+  /* Style for box shadow Lg */
   &.storybook-box--Lg {
     box-shadow: rgba(136, 165, 191, 0.509) 6px 2px 16px 0px, rgba(255, 255, 255, 0.8) -6px -2px 16px 0px;
   }
 
-  /* Gaya untuk box shadow xl */
+  /* Style for box shadow xl */
   &.storybook-box--xl {
     box-shadow: rgba(136, 165, 191, 0.48) 6px 2px 16px 0px, rgba(255, 255, 255, 0.8) -6px -2px 16px 0px;
   }
 
+  /* Style for box shadow xxl */
   &.storybook-box--xxl {
     box-shadow: rgba(0, 0, 0, 0.4) 0px 30px 90px;
   }
 
+  /* Style for box shadow Inner */
   &.storybook-box--Inner {
     box-shadow: rgba(0, 0, 0, 0.061) 0px 4px 12px 0px inset;
   }
 
-  /* Tambahkan gaya lain jika diperlukan */
+  /* Add another style if needed */
 `;
 
-export default Boxstyled;
+export default BoxStyled;
