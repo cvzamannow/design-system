0904ea2851f1799b2936870fbddeabe7005be06a
Roid-obi <contohdiamazon2@gmail.com>
2023-08-18 10:27:59 +0700

    added various features and fixed errors

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,21 @@
 import type { Preview } from "@storybook/react";
+import { create } from "@storybook/theming";
+
+const lightTheme = create({
+  base: "light",
+  appBg: "#F9F9F9",
+  colorPrimary: "#0D4A3E",
+  colorSecondary: "#FFA824",
+  barBg: "#F9F9F9",
+  brandImage: "https://github.com/Roid-obi/logo-cvzn-ds/blob/main/CV%20Zaman%20Now-oke.png?raw=true",
+});
+
+const darkTheme = create({
+  base: "dark",
+  colorPrimary: "#FFA824",
+  colorSecondary: "#3D6E65",
+  brandImage: "https://github.com/Roid-obi/logo-cvzn-ds/blob/main/CV%20Zaman%20Now-oke.png?raw=true",
+});
 
 const preview: Preview = {
   parameters: {
@@ -9,6 +26,11 @@ const preview: Preview = {
         date: /Date$/,
       },
     },
+    darkMode: {
+      dark: darkTheme,
+      light: lightTheme,
+      stylePreview: true,
+    }
   },
 };
 
