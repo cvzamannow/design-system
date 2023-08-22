7b27078eed5882e9c30b1d60ddf0b37b2d5946c5
Roid-obi <contohdiamazon2@gmail.com>
2023-08-22 11:26:13 +0700

    feat: create label

---
+++ src/stories/Label/Label.stories.ts
@@ -0,0 +1,96 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { within } from '@storybook/testing-library';
+import { expect } from "@storybook/jest";
+
+import { Label } from "./Label";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: "Stories/Label",
+  component: Label,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+} satisfies Meta<typeof Label>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const ExtraSmall: Story = {
+  args: {
+    size: "extraSmall",
+    label: "extraSmall",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const testLabel = await canvas.getByText('extraSmall');
+    const testSize = canvas.getByTestId('label-size-extraSmall');
+
+    await expect(testLabel).toBeInTheDocument();
+    await expect(testSize).toBeInTheDocument();
+  },
+};
+
+export const Small: Story = {
+  args: {
+    size: "small",
+    label: "Small",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const testLabel = await canvas.getByText('Small');
+    const testSize = canvas.getByTestId('label-size-small');
+
+    await expect(testLabel).toBeInTheDocument();
+    await expect(testSize).toBeInTheDocument();
+  },
+};
+
+export const Normal: Story = {
+  args: {
+    size: "normal",
+    label: "Normal",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const testLabel = await canvas.getByText('Normal');
+    const testSize = canvas.getByTestId('label-size-normal');
+
+    await expect(testLabel).toBeInTheDocument();
+    await expect(testSize).toBeInTheDocument();
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    size: "medium",
+    label: "Medium",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const testLabel = await canvas.getByText('Medium');
+    const testSize = canvas.getByTestId('label-size-medium');
+
+    await expect(testLabel).toBeInTheDocument();
+    await expect(testSize).toBeInTheDocument();
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: "large",
+    label: "Large",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const testLabel = await canvas.getByText('Large');
+    const testSize = canvas.getByTestId('label-size-large');
+
+    await expect(testLabel).toBeInTheDocument();
+    await expect(testSize).toBeInTheDocument();
+  },
+};
---
+++ src/stories/Label/Label.tsx
@@ -0,0 +1,23 @@
+import LabelStyled from "./label.styles";
+
+interface LabelProps {
+  /**
+   * How large should the Label be?
+   */
+  size?: "extraSmall" | "small" | "normal" | "medium" | "large";
+  /**
+   * contents
+   */
+  label: string;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Label = ({ size = "normal", label, ...props }: LabelProps) => {
+  return (
+    <LabelStyled data-testid={`label-size-${size}`} className={["storybook-label", `storybook-label--${size}`].join(" ")} {...props}>
+      {label}
+    </LabelStyled>
+  );
+};
