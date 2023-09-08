8e4fb7a8ddedf617d9fe16b5682eab38c879fada
Roid-obi <contohdiamazon2@gmail.com>
2023-09-08 19:27:36 +0700

    build: instruction storybook

---
+++ storybook-static/assets/index-f5b66f96.js
@@ -0,0 +1,2 @@
+import{M as r,a,u as m,w as n}from"./index-a74ed5fa.js";import"./index-e8534147.js";import"./_commonjsHelpers-de833af9.js";export{r as MDXContext,a as MDXProvider,m as useMDXComponents,n as withMDXComponents};
+//# sourceMappingURL=index-f5b66f96.js.map
