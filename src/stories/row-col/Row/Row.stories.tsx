2c864df45c48a637b1ec0b04d0df8e76de29859f
Roid-obi <contohdiamazon2@gmail.com>
2023-09-15 11:04:16 +0700

    feat: create docs component

--- src/stories/row-col/Row/Row.stories.tsx
+++ src/stories/row-col/Row/Row.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import { Title, Description, Subtitle, Stories, Primary as PrimaryDocBlock, Controls} from "@storybook/blocks"
 import Row from "./Row";
 import { ContentText } from "../../Card/card.styles";
 import Card from "../../Card/Card";
@@ -11,6 +12,19 @@ const meta = {
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: "centered",
+    componentSubtitle: "'Nuff Said",
+    docs: {
+      page: () => <>
+        <Title />
+        <Description />
+        <Subtitle />
+
+        <PrimaryDocBlock />
+        <Controls />
+
+        <Stories />
+      </>,
+    },
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ["autodocs"],
